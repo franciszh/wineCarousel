8c0ab7416111dbb5256696d9d82c097359636b11
Francis Zhao <francis_zyf@hotmail.com>
2025-01-12 21:31:41 +1100

    feat: TDD wine card html skeleton

---
+++ __test__/components/windCard.test.tsx
@@ -0,0 +1,67 @@
+import { render } from "@testing-library/react";
+import { WineCard } from "@/components/ui/wineCard";
+
+const baseProps = {
+    cardLink: "#",
+    productName: "Royal Solute",
+    productDescription: "Royal Salute 21 Year Old Blended Scotch Whisky Miami Polo Edition 700ml (Gift Box)",
+    productImage: "https://unsplash.it/200/200/?random",
+    addToCartCTA: () => {}
+};
+
+describe("WineCard", () => {
+    it("renders product name and description as passed in the props", () => {
+        const { getByText } = render(<WineCard {...baseProps}/>);
+        const name = getByText("Royal Solute");
+        const description = getByText("Royal Salute 21 Year Old Blended Scotch Whisky Miami Polo Edition 700ml (Gift Box)");
+        expect(name).toBeInTheDocument();
+        expect(description).toBeInTheDocument();
+    });
+
+    it("renders the accessible product image", () => {
+        const { getByAltText } = render(<WineCard {...baseProps}/>);
+        const productImage = getByAltText("Royal Solute product image");
+
+        expect(productImage).toBeInTheDocument();
+    });
+
+    it("renders the tag if it is available", () => {
+        const { getByText } = render(<WineCard {...baseProps} productTag="Sale"/>);
+        const tag = getByText("Sale");
+
+        expect(tag).toBeInTheDocument();
+    });
+
+    it("renders the current cash price solely if was cash price is nullish", () => {
+        const { getByText } = render(<WineCard {...baseProps} currentPriceCashAmount={500} currentPriceCashCurrencyCode="AUD"/>);
+        const currenCashPrice = getByText("$500");
+
+        expect(currenCashPrice).toBeInTheDocument();
+    });
+
+    it("renders the current cash price and was cash price if was cash price is not nullish in an accessible way", () => {
+        const { getByLabelText } = render(<WineCard {...baseProps} currentPriceCashAmount={500} currentPriceCashCurrencyCode="AUD" wasPriceCashAmount={600} wasPriceCashCurrencyCode="AUD"/>);
+        const wasCashPrice = getByLabelText("It was $600.");
+        const currentCashPrice = getByLabelText("Now only $500.");
+
+        expect(wasCashPrice).toBeInTheDocument();
+        expect(currentCashPrice).toBeInTheDocument();
+    });
+
+    it("renders the current points solely if was points is nullish", () => {
+        const { getByText } = render(<WineCard {...baseProps} currentPricePoints={5000}/>);
+        const currenPoints = getByText("5000 PTS");
+
+        expect(currenPoints).toBeInTheDocument();
+    });
+
+    it("renders the current points and was points if was points is not nullish in an accessible way", () => {
+        const { getByLabelText } = render(<WineCard {...baseProps} currentPricePoints={5000} wasPricePoints={6000}/>);
+        const currentPoints = getByLabelText("With 5000 PTS now.");
+        const wasPoints = getByLabelText("Discounted from 6000 PTS.");
+
+        expect(currentPoints).toBeInTheDocument();
+        expect(wasPoints).toBeInTheDocument();
+    });
+
+});
---
+++ components/ui/wineCard.tsx
@@ -0,0 +1,143 @@
+import * as React from "react";
+import Image from "next/image";
+
+interface WineCardProps {
+    cardLink: string;
+    productName: string;
+    productDescription: string;
+    productImage: string;
+    addToCartCTA: () => void;
+    currentPriceCashAmount?: number;
+    currentPriceCashCurrencyCode?: string;
+    wasPriceCashAmount?: number;
+    wasPriceCashCurrencyCode?: string;
+    wasPricePoints?: number;
+    productTag?: string;
+    currentPricePoints?: number;
+}
+
+type WineCardCashBlockProps = Pick<WineCardProps, "currentPriceCashAmount" | "currentPriceCashCurrencyCode" | "wasPriceCashAmount" | "wasPriceCashCurrencyCode">
+
+type WinCardPointsBlockProps = Pick<WineCardProps, "wasPricePoints" | "currentPricePoints">
+
+const currencySymbolMap: Record<string, string> = {
+    "AUD": "$"
+};
+
+const pointsUnit = "PTS";
+
+const WineCardCashBlock = (props: WineCardCashBlockProps) => {
+    const {currentPriceCashAmount, currentPriceCashCurrencyCode, wasPriceCashAmount, wasPriceCashCurrencyCode} = props;
+    const currentCurrencySymbol = currentPriceCashCurrencyCode ? currencySymbolMap[currentPriceCashCurrencyCode] : "";
+    const wasCurrencySymbol = wasPriceCashCurrencyCode ? currencySymbolMap[wasPriceCashCurrencyCode] : "";
+    
+    // two lines with wasPriceCash on the top
+    if (wasPriceCashAmount) {
+        const wasPrice = `${wasCurrencySymbol}${wasPriceCashAmount}`;
+        const currentPrice = `${currentCurrencySymbol}${currentPriceCashAmount}`;
+        return (
+            <>
+                <div aria-label={`It was ${wasPrice}.`}>
+                    {wasPrice}
+                </div>
+                <div aria-label={`Now only ${currentPrice}.`}>
+                    {currentPrice}
+                </div>
+            </>
+        )
+    }
+    // one line with currentPriceCash
+    return (
+        <>
+            <div>
+                {`${currentCurrencySymbol}${currentPriceCashAmount}`}
+            </div>
+        </>
+    )
+    
+}
+
+const WineCardPointsBlock = (props: WinCardPointsBlockProps) => {
+    const {wasPricePoints, currentPricePoints} = props;
+
+    // two lines with currentPricePoints on the top
+    if (wasPricePoints) {
+        const currentPoints = `${currentPricePoints} ${pointsUnit}`;
+        const wasPoints = `${wasPricePoints} ${pointsUnit}`;
+        return (
+            <>
+                <div>
+                    <span>
+                        Or
+                    </span>
+                    <span aria-label={`With ${currentPoints} now.`}>
+                        {currentPoints}
+                    </span>
+                </div>
+                <div aria-label={`Discounted from ${wasPoints}.`}>
+                    {wasPoints}
+                </div>
+            </>
+        )
+    }
+    // one line with currentPricePoints
+    return (
+        <>
+            <div>
+                <span>
+                    Or
+                </span>
+                <span>
+                    {`${currentPricePoints} ${pointsUnit}`}
+                </span>
+            </div>
+        </>
+    )
+}
+
+
+export const WineCard = (props: WineCardProps) => {
+    const { cardLink, productTag, productName, 
+            productDescription, productImage, 
+            currentPriceCashAmount, currentPriceCashCurrencyCode,
+            wasPriceCashAmount, wasPriceCashCurrencyCode,
+            wasPricePoints, currentPricePoints, addToCartCTA
+        } = props;
+
+    return (
+        <div>
+            <a href={cardLink} >
+                {productTag && <div>{productTag}</div>}
+                <div>
+                    <h4>
+                        {productName}
+                    </h4>
+                    <div>
+                        {productDescription}
+                    </div>
+                    <div>
+                        <Image
+                            src={productImage}
+                            alt={`${productName} product image`}
+                            width={200}
+                            height={200}
+                        />
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <WineCardCashBlock {...{currentPriceCashAmount, currentPriceCashCurrencyCode, wasPriceCashAmount, wasPriceCashCurrencyCode}}/>
+                    </div>
+                    <div>
+                        <WineCardPointsBlock {...{wasPricePoints, currentPricePoints}}/>
+                    </div>
+                </div>
+            </a>
+            <div>
+                <button type="button" onClick={addToCartCTA}>
+                    <span>ADD</span>
+                </button>
+            </div>
+        </div>
+    );
+}
